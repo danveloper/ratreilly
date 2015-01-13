c970b124fd42ad4961d92a4ae8e285741026dfea
Daniel Woods <daniel.p.woods@gmail.com>
2015-01-13 08:05:30 -0800

    DEMO #3 - Data flow between handlers with verbs
    * Run either the groovy-demo or the java-demo
      - Point browser to:
        > http://localhost:5050/
        > http://localhost:5050/route1
        > http://localhost:5050/route2/foo

--- groovy-demo/src/ratpack/ratpack.groovy
+++ groovy-demo/src/ratpack/ratpack.groovy
@@ -3,7 +3,14 @@ import static ratpack.groovy.Groovy.ratpack
 ratpack {
   handlers {
     get {
-      response.send "Hello World!"
+      response.send "I'm the default route!"
+    }
+    get("route1") {
+      response.send "I'm in route1"
+    }
+    get("route2/:param") {
+      response.send "I'm in route2 and received param: ${pathTokens.param}"
     }
   }
 }
+
--- java-demo/src/main/java/oreilly/JavaMain.java
+++ java-demo/src/main/java/oreilly/JavaMain.java
@@ -1,5 +1,6 @@
 package oreilly;
 
+import ratpack.error.internal.DefaultDevelopmentErrorHandler;
 import ratpack.server.RatpackServer;
 import ratpack.server.ServerConfig;
 
@@ -7,8 +8,18 @@ public class JavaMain {
 
   public static void main(String[] args) throws Exception {
     RatpackServer.start(spec -> spec
-        .config(ServerConfig.noBaseDir())
-        .handler(r -> (ctx) -> ctx.getResponse().send("Hello World!"))
+            .config(ServerConfig.noBaseDir().development(true))
+            .registry(r -> r.add(new DefaultDevelopmentErrorHandler()))
+            .handlers(chain -> chain
+                .get(ctx ->
+                    ctx.getResponse().send("I'm in the default route!"))
+
+                .get("route1", (ctx) ->
+                    ctx.getResponse().send("I'm in route1!"))
+
+                .get("route2/:param", (ctx) ->
+                    ctx.getResponse().send(String.format("received param: %s", ctx.getPathTokens().get("param"))))
+    )
     );
   }
 }
