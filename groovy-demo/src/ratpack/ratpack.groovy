e15b20562a60174a4a19d7362acae92cf15d5ebf
Daniel Woods <daniel.p.woods@gmail.com>
2015-01-13 08:17:21 -0800

    DEMO #5 - Handler data flow with byMethod
    * Start either groovy-demo or java-demo
      - Point browser to http://localhost:5050
        > Should see the "transaction" printlns in the console

--- groovy-demo/src/ratpack/ratpack.groovy
+++ groovy-demo/src/ratpack/ratpack.groovy
@@ -1,11 +1,35 @@
+import ratpack.error.internal.DefaultDevelopmentErrorHandler
+
+
 import static ratpack.groovy.Groovy.ratpack
 
+class Transactions {
+  void beginTransaction() {
+    println "Beginning Transaction"
+  }
+  void endTransaction() {
+    println "Ending Transaction"
+  }
+}
+
 ratpack {
+  bindings {
+    bindInstance(new Transactions())
+    bindInstance(new DefaultDevelopmentErrorHandler())
+  }
   handlers {
-    get "route1", {
-      response.send "I'm in route1!"
+    handler { Transactions tx ->
+      tx.beginTransaction()
+      byMethod {
+        get {
+          response.send "Default GET handler"
+        }
+        post {
+          response.send "You POSTED to me!"
+        }
+      }
+      tx.endTransaction()
     }
-    assets("public", "index.html")
   }
 }
 
--- java-demo/src/main/java/oreilly/JavaMain.java
+++ java-demo/src/main/java/oreilly/JavaMain.java
@@ -6,15 +6,38 @@ import ratpack.server.ServerConfig;
 
 public class JavaMain {
 
+  static class Transactions {
+    void beginTransaction() {
+      System.out.println("Transaction initiated!");
+    }
+
+    void endTransaction() {
+      System.out.println("Transaction ended!");
+    }
+  }
+
   public static void main(String[] args) throws Exception {
     RatpackServer.start(spec -> spec
             .config(ServerConfig.findBaseDirProps("ratpack.properties").development(true))
-            .registry(r -> r.add(new DefaultDevelopmentErrorHandler()))
+            .registry(r -> {
+              r.add(new Transactions());
+              r.add(new DefaultDevelopmentErrorHandler());
+            })
             .handlers(chain -> chain
-                .get("/route1", ctx ->
-                    ctx.getResponse().send("I'm in route1!"))
-                .assets("public", "index.html")
-    )
+                    .handler(ctx -> {
+                      Transactions tx = ctx.get(Transactions.class);
+                      tx.beginTransaction();
+                      ctx.byMethod(m -> m
+                              .get(() ->
+                                      ctx.getResponse().send("Default GET handler")
+                              )
+                              .post(() ->
+                                      ctx.getResponse().send("You POSTed to me!")
+                              )
+                      );
+                      tx.endTransaction();
+                    })
+            )
     );
   }
 }
